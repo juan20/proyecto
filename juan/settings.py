00f2d2f8b44199b78e0d5129bcccc5fbf2ab498c
juan20 <jguillent20@gmail.com>
2017-07-07 21:30:16 -0400

    agragacion de jet

--- juan/settings.py
+++ juan/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    
+    #'jet.dashboard',
+    'jet',
     'main',
 ]
 
--- juan/urls.py
+++ juan/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url, include
+from django.conf.urls import include, url 
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from tastypie.api import Api
@@ -28,8 +28,11 @@ entry_resource = EntryResource()
 receive_resource = ReceiveResource()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(receive_resource.urls)),
-    url(r'^api/', include(entry_resource.urls)),
-    url(r'^', index),
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    url(r'^admin/', include(admin.site.urls)),
+
+    # url(r'^api/', include(receive_resource.urls)),
+    # url(r'^api/', include(entry_resource.urls)),
+    url(r'^$', index),
 ] + staticfiles_urlpatterns()
