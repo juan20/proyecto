c783530f263730d9c4c6ed5da9b4d303a3990f88
rramoso <rramoso@users.noreply.github.com>
2017-07-16 15:59:06 -0400

    Update settings.py

--- juan/settings.py
+++ juan/settings.py
@@ -124,5 +124,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'juan/static'),
 )
